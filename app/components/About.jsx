c1e009507f61a0ca17e55a820fc1dfdc4e87d488
Truong Minh Nhat Quang <nhatquangsin@gmail.com>
2017-06-09 20:04:24 +0700

    add css style

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,13 +3,13 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h1 className="text-center">About</h1>
+            <h1 className="text-center page-title">About</h1>
             <p>This is a React application build on React.</p>
             <p>This is some of the tools I used:</p>
             <ul>
                 <li>
                     <a href="https://facebook.github.io/react">React</a>
-                    -This was the Javascript framework
+                    -This was the Javascript framework used
                 </li>
                 <li>
                     <a href="http://openweathermap.org">Open weather map</a>
